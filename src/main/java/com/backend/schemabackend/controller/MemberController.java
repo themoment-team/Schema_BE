b55da248d27f1fef14de2e1e8748e854952d0ce9
hajeu <s22072@gsm.hs.kr>
2022-10-31 15:48:44 +0900

    troubleShootingCommit

--- src/main/java/com/backend/schemabackend/controller/MemberController.java
+++ src/main/java/com/backend/schemabackend/controller/MemberController.java
@@ -10,10 +10,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +25,7 @@ import java.util.Optional;
 @ResponseBody
 @Slf4j
 public class MemberController {
+
     @Autowired
     private MemberService memberService;
     @Autowired
